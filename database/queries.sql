aebe7a9c0f11af6f1cabdcd0c6c7736bcad75149
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-12-13 02:28:52 -0200

    Inserção de Query 4 e 5.

--- database/queries.sql
+++ database/queries.sql
@@ -1,19 +1,37 @@
 use Spotify;
 
 /* QUERY 1 */
-Artista mais ouvido pelo usuário X (artista, musica, usuario)
-select * from Musica;
+-- Artista mais ouvido pelo usuário X (artista, musica, reproduções, usuario)
 
 /* QUERY 2 */
-Artista mais famoso das playlists (Playlist, Musica, Artista)
+-- Artista mais ouvido das playlists (Playlist, Musica, Artista)
 
 /* QUERY 3 */
-Concertos do artista X mais proximos do usuário Y (Concerto, artista, usuario)
+-- Concertos do artista X no mesmo país do usuário Y (Concerto, artista, usuario)
 
 /* QUERY 4 */
-Todas as musicas do genero rock (Genero, artista, musica)
+-- As cidades onde a música 'Get Lucky' é ouvida com o número de reproduções,
+-- ordenando pelo número de reproduções em forma decrescente.
+SELECT count(cidade) as reproducoes, cidade
+FROM Reproducao
+JOIN Musica ON (Musica.titulo = 'Get Lucky' AND Reproducao.id_musica = Musica.id_musica)
+JOIN Usuario USING (id_usuario)
+GROUP BY cidade
+ORDER BY reproducoes DESC
 
 /* QUERY 5 */
-
+-- Todas as músicas do gênero Rock (Gênero, Categorização, álbum, música)
+select *
+from Musica
+WHERE Musica.id_album IN
+(
+  SELECT Album.id_album
+  FROM Album
+  JOIN Categorizacao ON
+    (Categorizacao.id_album = Album.id_album)
+  JOIN Genero ON
+    (Genero.nome = 'Rock' AND Categorizacao.id_genero = Genero.id_genero)
+);
 
 /* QUERY 6 */
+--
--- server/comandos.js
+++ server/comandos.js
@@ -18,13 +18,34 @@ module.exports = (app, connection) => {
   });
 
   app.get('/query/4', (req, res) => {
-    connection.query('SELECT * FROM Usuario', (error, results, fields) => {
+    connection.query(
+      `
+        SELECT count(cidade) as reproducoes, cidade
+        FROM Reproducao
+        JOIN Musica ON (Musica.titulo = 'Get Lucky' AND Reproducao.id_musica = Musica.id_musica)
+        JOIN Usuario USING (id_usuario)
+        GROUP BY cidade
+        ORDER BY reproducoes DESC
+      `, (error, results, fields) => {
       res.send({ error, results, fields });
     });
   });
 
   app.get('/query/5', (req, res) => {
-    connection.query('SELECT * FROM Radio', (error, results, fields) => {
+    connection.query(
+      `
+        select *
+        from Musica
+        WHERE Musica.id_album IN
+        (
+          SELECT Album.id_album
+          FROM Album
+          JOIN Categorizacao ON
+            (Categorizacao.id_album = Album.id_album)
+          JOIN Genero ON
+            (Genero.nome = 'Rock' AND Categorizacao.id_genero = Genero.id_genero)
+        );
+      `, (error, results, fields) => {
       res.send({ error, results, fields });
     });
   });
