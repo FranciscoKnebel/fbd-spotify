0ba5d9a625c4144d218ece5c264f68d580cc682f
Mateus Salvi <34428544+Teuszila@users.noreply.github.com>
2017-12-13 00:25:41 -0200

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,14 +1,19 @@
 use Spotify;
 
 /* QUERY 1 */
+Artista mais ouvido pelo usuário X (artista, musica, usuario)
 select * from Musica;
 
 /* QUERY 2 */
+Artista mais famoso das playlists (Playlist, Musica, Artista)
 
 /* QUERY 3 */
+Concertos do artista X mais proximos do usuário Y (Concerto, artista, usuario)
 
 /* QUERY 4 */
+Todas as musicas do genero rock (Genero, artista, musica)
 
 /* QUERY 5 */
+
 
 /* QUERY 6 */
